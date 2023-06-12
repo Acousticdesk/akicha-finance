2487d8c45c918bed937ec46ecb6066377a782a39
Andrii Kicha <akicha@star.global>
2023-06-12 10:20:16 -0600

    add ui for the create expense screen

--- src/expenses/components/CreateExpense.tsx
+++ src/expenses/components/CreateExpense.tsx
@@ -1,3 +1,36 @@
+import { useRef, useEffect } from "react";
+import { Container, Button, Flex, Input } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+
 export function CreateExpense() {
-  return "A new expense";
+  const inputRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+  function handleSubmit() {
+    navigate("/");
+  }
+  return (
+    <Container>
+      <Flex direction="column" h="100vh" justify="space-between" py={4}>
+        <Input
+          display="block"
+          mx="auto"
+          w="160px"
+          ref={inputRef}
+          type="number"
+          name="amount"
+          onKeyUp={(e) => {
+            if (e.code === "Enter") {
+              handleSubmit();
+            }
+          }}
+        />
+        <Button colorScheme="green" onClick={handleSubmit}>
+          Add
+        </Button>
+      </Flex>
+    </Container>
+  );
 }
