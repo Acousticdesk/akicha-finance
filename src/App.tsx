4971d0008fdfdd8d62e6b416510c4c6c06b67a43
Andrii Kicha <akicha@star.global>
2023-06-20 13:14:29 -0600

    add header to the main screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import {
-  Container,
-  Box,
-  Heading,
-  Flex,
-  Spacer,
-  IconButton,
-} from "@chakra-ui/react";
-import { AddIcon } from "@chakra-ui/icons";
+import { Container, Box, Heading, Flex, IconButton } from "@chakra-ui/react";
+import { AddIcon, HamburgerIcon } from "@chakra-ui/icons";
 import { useNavigate } from "react-router-dom";
 import { ExpensesStripe, ExpensesCategoryList } from "./expenses";
 
@@ -14,30 +7,39 @@ export function App() {
   const navigate = useNavigate();
 
   return (
-    <Container h="100%">
-      <Flex flexDirection="column" h="100%" pb={24}>
+    <Box h="100%">
+      <Flex flexDirection="column" h="100%" pb={24} justify="space-between">
         <header>
-          <Heading size="lg">June</Heading>
+          <Box p={4} background="green.400">
+            <IconButton
+              colorScheme="black"
+              variant="outline"
+              icon={<HamburgerIcon />}
+              aria-label="Open Menu"
+            />
+          </Box>
         </header>
-        <ExpensesStripe />
-        <Spacer />
-        <Box my={4}>
-          <ExpensesCategoryList />
-        </Box>
+        <Container>
+          <Heading size="lg">June</Heading>
+          <ExpensesStripe />
+          <Box mt={8} mb={4}>
+            <ExpensesCategoryList />
+          </Box>
+          <IconButton
+            position="fixed"
+            bottom={2}
+            right={2}
+            w={20}
+            h={20}
+            fontSize={20}
+            borderRadius="50%"
+            aria-label="Add expenses"
+            icon={<AddIcon />}
+            colorScheme="red"
+            onClick={() => navigate("/create-expense")}
+          />
+        </Container>
       </Flex>
-      <IconButton
-        position="fixed"
-        bottom={2}
-        right={2}
-        w={24}
-        h={24}
-        fontSize={24}
-        borderRadius="50%"
-        aria-label="Add expenses"
-        icon={<AddIcon />}
-        colorScheme="red"
-        onClick={() => navigate("/create-expense")}
-      />
-    </Container>
+    </Box>
   );
 }
