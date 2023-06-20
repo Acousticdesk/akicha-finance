6d9d7a6a746efe7b8c70af74dad9f151a650f207
Andrii Kicha <akicha@star.global>
2023-06-20 12:29:52 -0600

    make dial keyboard work

--- src/expenses/components/CreateExpense.tsx
+++ src/expenses/components/CreateExpense.tsx
@@ -7,16 +7,37 @@ import {
 } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { Dial } from "./Dial.tsx";
+import { useState } from "react";
 
 export function CreateExpense() {
   const navigate = useNavigate();
+  const [expenseInputValue, setExpenseInputValue] = useState("");
   function handleSubmit() {
     navigate("/");
+  }
+  function handleDialClick(e: MouseEvent) {
+    const target = e.target as HTMLButtonElement;
+
+    if (target.dataset.dial) {
+      const value = target.dataset.dial;
+      setExpenseInputValue((previousValue) => previousValue + value);
+    }
+
+    if (target.dataset.action) {
+      const action = target.dataset.action;
+
+      if (action === "delete") {
+        setExpenseInputValue((previousValue) =>
+          previousValue.substring(0, previousValue.length - 1)
+        );
+      }
+    }
   }
   return (
     <Container h="100%">
       <Flex direction="column" justify="space-between" pt={4} pb={8} h="100%">
         <NumberInput
+          value={expenseInputValue}
           isReadOnly
           display="block"
           mx="auto"
@@ -30,7 +51,7 @@ export function CreateExpense() {
         >
           <NumberInputField h={24} fontSize={40} textAlign="center" />
         </NumberInput>
-        <Dial />
+        <Dial onClick={handleDialClick} />
         <Button h={16} colorScheme="green" onClick={handleSubmit}>
           Add
         </Button>
--- src/expenses/components/Dial.tsx
+++ src/expenses/components/Dial.tsx
@@ -1,19 +1,33 @@
 import { Flex, Button, Box, Text } from "@chakra-ui/react";
+import { MouseEventHandler } from "react";
 
-export function Dial() {
+interface DialProps {
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export function Dial({ onClick }: DialProps) {
+  const actionIcons: { [key: string]: string } = {
+    delete: "⬅️",
+  };
   return (
-    <Flex wrap="wrap" justify="center">
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((key) => {
+    <Flex wrap="wrap" justify="right">
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "delete"].map((key) => {
         return (
-          <Box position="relative" w="30.3%" mx="1.5%" my="1.5%">
-            <Button w="100%" pb="100%" key={key} />
+          <Box key={key} position="relative" w="30.3%" mx="1.5%" my="1.5%">
+            <Button
+              w="100%"
+              pb="100%"
+              onClick={onClick}
+              data-dial={typeof key === "number" ? key : null}
+              data-action={typeof key === "string" ? key : null}
+            />
             <Box
               position="absolute"
               left="50%"
               top="50%"
               transform="translate(-50%, -50%)"
             >
-              <Text>{key}</Text>
+              <Text>{typeof key === "number" ? key : actionIcons[key]}</Text>
             </Box>
           </Box>
         );
